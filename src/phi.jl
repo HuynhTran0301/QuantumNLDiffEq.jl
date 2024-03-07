b80ee0d4fa32a2082512bae316c9fcb1dddbc2a5
Huynh Tran <115194407+HuynhTran0301@users.noreply.github.com>
2024-03-07 14:41:05 -0500

    Update phi.jl

--- src/phi.jl
+++ src/phi.jl
@@ -2,7 +2,11 @@ function phi(x, ::Product)
 	return asin(x)
 end
 
-function phi(x, mapping::Union{ChebyshevSparse, ChebyshevTower})
-	return mapping.pc*acos(x)
+function phi(x, mapping::ChebyshevSparse)
+    return mapping.pc*acos(x)
+end
+
+function phi(x, mapping::ChebyshevTower)
+    return mapping.pc*acos(x)
 end
 
