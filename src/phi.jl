4b28592dbaeeb21681387f9424bbf9eae7decb1b
Huynh Tran <115194407+HuynhTran0301@users.noreply.github.com>
2024-03-08 12:05:35 -0500

    Update phi.jl

--- src/phi.jl
+++ src/phi.jl
@@ -1,32 +1,24 @@
 using Zygote
 
+# Define phi functions for each type
 function phi(x, mapping::Product)
-	return 2*asin(x)
+    return 2 * asin(x)
 end
-function grad_phi_with_respect_to_x(x, mapping::Product)
-    # Actual gradient calculation specific to your operation
-    return mapping.pc / sqrt(1 - x^2)
-end
-@adjoint phi(x, mapping::Product) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
-
-
 
 function phi(x, mapping::ChebyshevSparse)
-	return 2*acos(x)
+    return 2 * acos(x)
 end
-function grad_phi_with_respect_to_x(x, mapping::ChebyshevSparse)
-    # Actual gradient calculation specific to your operation
-    return -mapping.pc / sqrt(1 - x^2)
-end
-@adjoint phi(x, mapping::ChebyshevSparse) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
-
 
 function phi(x, mapping::ChebyshevTower)
-	return mapping.pc*acos(x)
+    return mapping.pc * acos(x)
 end
 
-function grad_phi_with_respect_to_x(x, mapping::ChebyshevTower)
-    # Actual gradient calculation specific to your operation
-    return -mapping.pc / sqrt(1 - x^2)
-end
-@adjoint phi(x, mapping::ChebyshevTower) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
+# Define gradient functions with proper method dispatching
+grad_phi_with_respect_to_x(x, mapping::Product) = 2 / sqrt(1 - x^2)
+grad_phi_with_respect_to_x(x, mapping::ChebyshevSparse) = -2 / sqrt(1 - x^2)
+grad_phi_with_respect_to_x(x, mapping::ChebyshevTower) = -mapping.pc / sqrt(1 - x^2)
+
+# Define adjoints for phi with proper type dispatching
+Zygote.@adjoint phi(x, mapping::Product) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
+Zygote.@adjoint phi(x, mapping::ChebyshevSparse) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
+Zygote.@adjoint phi(x, mapping::ChebyshevTower) = phi(x, mapping), Δ -> (Δ * grad_phi_with_respect_to_x(x, mapping), nothing)
