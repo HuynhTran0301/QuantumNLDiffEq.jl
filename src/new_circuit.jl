ce94cd190b048ac011a3f88e33f50c782e822ec0
Huynh Tran <115194407+HuynhTran0301@users.noreply.github.com>
2024-03-08 09:10:49 -0500

    Update new_circuit.jl

--- src/new_circuit.jl
+++ src/new_circuit.jl
@@ -1,17 +1,17 @@
-function load(x, N, mapping::Product)
-	return repeat([phi(x, mapping)], N)
+function load(x, N, mapping::Product, pc)
+	return repeat([phi(x, mapping, pc)], N)
 end
 
-function load(x, N, mapping::ChebyshevSparse)
-	return repeat([phi(x, mapping)], N)
+function load(x, N, mapping::ChebyshevSparse, pc)
+	return repeat([phi(x, mapping, pc)], N)
 end
 
-function load(x, N, mapping::ChebyshevTower)
-	return [i*phi(x, mapping) for i in 1:N]
+function load(x, N, mapping::ChebyshevTower, pc)
+	return [i*phi(x, mapping, pc) for i in 1:N]
 end
 
 function new_circuit(DQC::DQCType, x, theta, n=1, v=0)
-	tmp = load(x, DQC.N, DQC.afm)
+	tmp = load(x, DQC.N, DQC.afm, DQC.pc)
 	f = [i == n ? tmp[i] .+ v : tmp[i] for i in 1:DQC.N]
 	
 	return chain(DQC.N, dispatch(DQC.fm, f), DQC.evol, dispatch(DQC.var, theta))
