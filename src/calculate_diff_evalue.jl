6f5190566f01b71f044fdf8c51d0453cc7884656
Huynh Tran <115194407+HuynhTran0301@users.noreply.github.com>
2024-03-08 10:12:11 -0500

    Update calculate_diff_evalue.jl

--- src/calculate_diff_evalue.jl
+++ src/calculate_diff_evalue.jl
@@ -11,7 +11,7 @@ function calculate_diff_evalue(DQC::DQCType, cost::Vector{<:AbstractBlock}, thet
 	for j in 1:DQC.N
 		diff_evalue_pos = expect(Add(cost), zero_state(DQC.N) => new_circuit(DQC, x, theta, j, pi/2))
 		diff_evalue_neg = expect(Add(cost), zero_state(DQC.N) => new_circuit(DQC, x, theta, j, -pi/2))
-		diff_evalue += (diff_evalue_pos - diff_evalue_neg) * map_to_circuit(gradient(_x -> phi(_x, DQC.N-1, DQC.afm), x)[1], j, DQC.afm)
+		diff_evalue += (diff_evalue_pos - diff_evalue_neg) * map_to_circuit(gradient(_x -> phi(_x, DQC.afm), x)[1], j, DQC.afm)
 	end
 	diff_evalue /= 2
 	return real(diff_evalue)
