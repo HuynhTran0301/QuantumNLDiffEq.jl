91acbc846c7c70f8230bc2e55058ffbf56e586be
Huynh Tran <115194407+HuynhTran0301@users.noreply.github.com>
2024-03-08 09:25:45 -0500

    Update phi.jl

--- src/phi.jl
+++ src/phi.jl
@@ -1,12 +1,12 @@
-function phi(x, ::Product, pc)
+function phi(x, pc, ::Product)
 	return pc*asin(x)
 end
 
-function phi(x, ::ChebyshevSparse, pc)
+function phi(x, pc, ::ChebyshevSparse)
 	return pc*acos(x)
 end
 
-function phi(x, ::ChebyshevTower, pc)
+function phi(x, pc, ::ChebyshevTower)
 	return pc*acos(x)
 end
 
